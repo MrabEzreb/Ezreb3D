8546cda253104f8efcf20864fafbb56ec25e65a9
MrabEzreb <zerbebram@gmail.com>
2014-11-14 13:30:51 -0500

    I FINNALLY figured out how to make it render correctly! YAY!

---
+++ src/com/ezreb/ezreb3D/renderer/GridFrame.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package com.ezreb.ezreb3D.renderer;
+
+import java.awt.Component;
+
+import com.ezreb.ezreb3D.XYZPoint;
+
+/**
+ * @author bram.zerbe
+ *
+ */
+public class GridFrame extends Component {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -2601455465557229811L;
+
+	/**
+	 * 
+	 */
+	public GridFrame(XYZPoint camPos, ) {
+		
+	}
+
+}
